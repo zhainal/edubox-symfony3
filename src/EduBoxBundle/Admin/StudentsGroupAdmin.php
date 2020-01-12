4b723cd2bed8e70ee57ce24b2bb8bd546f8a0aa5
Agoyli <agoyliq@ya.ru>
2020-01-12 07:35:38 +0000

    Fix bug

--- src/EduBoxBundle/Admin/StudentsGroupAdmin.php
+++ src/EduBoxBundle/Admin/StudentsGroupAdmin.php
@@ -4,7 +4,6 @@
 namespace EduBoxBundle\Admin;
 
 
-use EduBoxBundle\Event\AdminEvent;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -15,17 +14,6 @@ class StudentsGroupAdmin extends AbstractAdmin
     protected $baseRouteName = 'edubox.admin.students_group';
     protected $studentClassRepository;
 
-    public function __construct($code, $class, $baseControllerName, EventDispatcherInterface $eventDispatcher)
-    {
-        $event = new AdminEvent(['studentClassRepository' => &$this->studentClassRepository]);
-        $eventDispatcher->dispatch(AdminEvent::STUDENT_ADMIN_PRE_CREATE, $event);
-        parent::__construct($code, $class, $baseControllerName);
-    }
-
-    public function getStudentClassRepository()
-    {
-        return $this->studentClassRepository;
-    }
 
     protected function configureRoutes(RouteCollection $collection)
     {
